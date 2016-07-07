49978ac4b17f6e342ddafc3e200d37a6b580073e
Boris <hanbk79410@gmail.com>
2016-07-07 04:44:21 +0000

    deleete

--- project/require/check.php
+++
@@ -1,5 +0,0 @@
-<?php 
-if(!isset($_COOKIE['userName']))
-    header("location: login.php");
-    
-?>
--- project/require/checkaccount.php
+++
@@ -1,9 +0,0 @@
-<?php 
-require_once("dbconnect.php");
-$command = "select aAccount,aPassword from account where aAccount='boris'";
-mysql_query($command,$link);
-echo mysql_query($command,$link);
-
-
-
-?>
