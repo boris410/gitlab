4ccd04ddfafa20dd2ccc3d1fa248a4cca6ce988c
Boris <hanbk79410@gmail.com>
2016-08-08 22:22:33 +0800

    view account 查詢 提款 存款 按鈕

--- payment/views/account.php
+++ payment/views/account.php
@@ -1,1 +1,7 @@
-<?php var_dump($data)?>
+
+<form action="" method="POST">
+    <H3>帳號 : <?php print_r($data[0]['account_account'])?><H3>
+    <input type="submit" value="查詢餘額" name="inquire"/>
+    <input type="submit" value="匯入金錢" name="input"/>
+    <input type="submit" value="提領現金" name="output"/>
+</form>
