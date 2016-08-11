d72a0c9c5be24ccab13d83545a7336933345478a
Boris <hanbk79410@gmail.com>
2016-08-12 00:49:30 +0800

    debug&change name ShowAccountDetail.php->ShowAccountRecord.php

--- payment/views/ShowAccountDetail.php
+++ payment/views/ShowAccountDetail.php
@@ -1,11 +1,11 @@
 <form action="" method="POST" class="output">
-    <?php foreach($data as $key => $value){ ?>
+    <?php foreach($data as  $value){ ?>
     <h3><tr>
-        <td>操作單號 : <?php echo $data[$key]['record_id']?></td>
-        <td>操作 : <?php echo $data[$key]['account_operation']?></td>
-        <td>操作金額 : <?php echo $data[$key]['account_opertaion_money']?></td>
-        <td>剩餘金額 : <?php echo $data[$key]['account_last_money']?></td>
-        <td>操作時間 : <?php echo $data[$key]['account_operation_time']?></td>
+        <td>操作單號 : <?php echo $value['record_id']?></td>
+        <td>操作 : <?php echo $value['account_operation']?></td>
+        <td>操作金額 : <?php echo $value['account_opertaion_money']?></td>
+        <td>剩餘金額 : <?php echo $value['account_last_money']?></td>
+        <td>操作時間 : <?php echo $value['account_operation_time']?></td>
     </tr>
     </h3>
     <br>
