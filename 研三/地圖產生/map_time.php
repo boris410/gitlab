6203801773ab8e295c418a3d017c5a3eddfdb057
Boris <hanbk79410@gmail.com>
2016-08-22 16:47:47 +0800

    新增map_time.php

--- 研三/地圖產生/map_time.php
+++ 研三/地圖產生/map_time.php
@@ -7,8 +7,8 @@ $num = 0;
 //產生40個不重覆亂數
 while($num < 1200){
     //0~9的固定亂數
-    $col = rand(0,59);
-    $row = rand(0,49);
+    $col = rand(0,49);
+    $row = rand(0,59);
     if (!$map[$row][$col]) {
         $map[$row][$col] = "M";
         $num++;
@@ -68,50 +68,15 @@ for ($x=0;$x<=59;$x++) {
         }
     }
 }
-
-印出結果到格子內
-echo <<< html
-<script type="text/javascript" src="jquery-3.1.0.js"></script>
-<script type="text/javascript" src="megamenu.js"></script>
-<body>
-<form >
-<table border=1>
-html;
-
-        for ($x=0;$x<=59;$x++) {
-            echo "<tr>";
-            for ($y=0;$y<=49;$y++) {
-                echo "<td>";
-                //echo "<input type=button style=width:120px;height:40px;font-size:20px;onclick=alert(click event occured)>";
-                print_r($map[$x][$y]);
-                echo "</td>";
-            }
-            echo "</tr>";
-        }
-echo <<< scri
-    <br>
-    </table>
-</form>
-        <script type=text/javascript>;
-
-        </script>
-</body>
-</html>
-scri;
-
-印出字串
-
+//印出字串
 for ($x=0;$x<=59;$x++) {
-    for ($y=0;$y<=4;$y++) {
+    for ($y=0;$y<=49;$y++) {
         print_r($map[$x][$y]);
     }
-    if ($x<9) {
-        echo "n";
+    if ($x<49) {
+        echo "N";
     }
 }
-echo "<br>";
-$iTime2 = microtime(true);
-echo "m=$num";
-echo "<br>";
-echo $iTime2-$iTime1;
+
+
 ?>
